0709bff214b1d13b50e4dac20be18bcc6f7f9382
Thenuwara <pramukhapoo@gmail.com>
2024-06-15 00:22:14 +0530

    edi

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,19 @@ import ContentSection from './ContentSection';
 import FeatureSection from './FeatureSection';
 import ServiceSection from './ServiceSection';
 import TestimonialSection from './testimonialSection';
+import Header from './Header';
+import Footer from './Footer';
 
 const Home = () => {
   return (
     <div className="home">
-
+      <Header />
       <HeroSection />
       <ContentSection />
       <ServiceSection />
       <FeatureSection />
       <TestimonialSection />
+      <Footer />
     
       
       {/* Add more sections as needed */}
