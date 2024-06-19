65051f1ac5b05c38608ab91cc031f17379b96d48
Thenuwara <pramukhapoo@gmail.com>
2024-06-19 14:54:39 +0530

    create litle bit dashbord part

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SideBarnNavbar from './SideBarnNavbar';
+import DashboardContent from './DashboardContent';
 
 const Dashboard = () => {
   return (
     <div className="">
       <SideBarnNavbar />
+      <DashboardContent />
     
       
       {/* Add more sections as needed */}
