627855d00ea5324656bc7a702a1e41429dd433d0
Thenuwara <pramukhapoo@gmail.com>
2024-06-23 02:22:40 +0530

    studyroom view done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
+import StudyRooms from './components/StudyRooms';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 import ContentsSection from './components/ContentSection';
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Dashboard />} />
+          <Route path="/studyrooms" element={<StudyRooms />} />
 
         </Routes>
       </div>
