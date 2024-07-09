78666ea8da0c6c89f966b9ac1d593ed28a9a0d06
Thenuwara <pramukhapoo@gmail.com>
2024-07-10 00:48:34 +0530

    forum page done but not responsive

--- src/components/Forums.jsx
+++ src/components/Forums.jsx
@@ -1,133 +1,276 @@
-import React from 'react';
-import SideBarnNavbar from './SideBarnNavbar';
+import React from "react";
+import SideBarnNavbar from "./SideBarnNavbar";
 
 const StudyRooms = () => {
   return (
     <div className="">
       <SideBarnNavbar />
-      <div className="xl:flex
-    mt-2 xl:ml-[263px] ml-[60px]">
-      <div className="xl:w-[75%]">
-        <div className="ml-3">
-          <div className="flex justify-between mt-5">
-            <div>
-              <div className="flex catergoryselect w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm"><p>General</p></div>
+      <div
+        className="xl:flex
+    mt-2 xl:ml-[263px] ml-[60px]"
+      >
+        <div className="xl:w-[75%]">
+          <div className="ml-3">
+            <div className="flex justify-between mt-5">
+              <div>
+                <div className="flex catergoryselect w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">
+                  <p>General</p>
+                </div>
+              </div>
+              <div>
+                <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">
+                  Computer S.
+                </div>
+              </div>
+              <div>
+                <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">
+                  Law
+                </div>
+              </div>
+              <div>
+                <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">
+                  Biology
+                </div>
+              </div>
+              <div>
+                <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">
+                  Chemistry
+                </div>
+              </div>
+              <div>
+                <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">
+                  Engineering
+                </div>
+              </div>
+              <div>
+                <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">
+                  Medicine
+                </div>
+              </div>
+              <div className="flex justify-center items-center mr-2">
+                <p className="catergoryarrow">
+                  <i class="fa-solid fa-angle-right text-3xl"></i>
+                </p>
+              </div>
             </div>
-            <div>
-              <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">Computer S.</div>
-            </div>
-            <div>
-              <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">Law</div>
-            </div>
-            <div>
-              <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">Biology</div>
-            </div>
-            <div>
-              <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">Chemistry</div>
-            </div>
-            <div>
-              <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">Engineering</div>
-            </div>
-            <div>
-              <div className="flex catergory w-[115px] h-[40px] rounded-[20px] justify-center items-center text-sm">Medicine</div>
-            </div>
-            <div className="flex justify-center items-center mr-2">
-              <p className="catergoryarrow"><i class="fa-solid fa-angle-right text-3xl"></i></p>
+            <div className="flex items-center justify-center mt-12 ml-12">
+              <div className="relative flex items-center">
+                <i class="fa-solid fa-magnifying-glass absolute ml-5 pointer-events-none text-xl"></i>
+                <input
+                  className="bg-search  border-none pr-3 pl-14  xl:w-[700px] h-[50px] rounded-[40px] mr-20 placeholder-black text-lg"
+                  name="search"
+                  placeholder="Search Questions"
+                  autocomplete="off"
+                  aria-label="Search Study Rooms"
+                ></input>
+              </div>
             </div>
           </div>
-          <div className="flex items-center justify-center mt-12 ml-12">
-            <div className="relative flex items-center">
-            <i class="fa-solid fa-magnifying-glass absolute ml-5 pointer-events-none text-xl"></i>
-            <input className="bg-search  border-none pr-3 pl-14  xl:w-[700px] h-[50px] rounded-[40px] mr-20 placeholder-black text-lg" name="search" placeholder="Search Questions" autocomplete="off" aria-label="Search Study Rooms"></input>
+          <div className="ml-20 mr-20 mt-12">
+            <div className="flex rounded-[20px] w-[100%] h-[350px]">
+              <div className="flex  justify-between items-center bg-questions w-[30%] mt-10 mb-10 ml-5 rounded-l-[30px]">
+                <div></div>
+                <div className="">
+                  <div className="flex justify-end mb-[-15px]">
+                    <p className="text-5xl font-semibold mr-2">Throw</p>
+                  </div>
+                  <div className="flex justify-end">
+                    <p className="text-5xl font-semibold mr-2 text-white">
+                      Your
+                    </p>
+                  </div>
+                  <div className="flex justify-end mt-[-15px]">
+                    <p className="text-5xl font-semibold mr-2">Questions</p>
+                  </div>
+                </div>
+              </div>
+              <div className="shadow-lg p-5 rounded-3xl">
+                <div class="flex items-center h-[25%] ">
+                  <select class="border questions rounded-2xl p-2 ">
+                    <option value="">Select a catergory</option>
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                  </select>
+                </div>
+                <div className="flex justify-center items-center h-[50%]">
+                  <textarea
+                    className="border questions rounded-[20px] h-[140px] w-[550px] p-5 "
+                    placeholder="Type your question here"
+                  ></textarea>
+                </div>
+                <div class="flex justify-between items-center h-[25%]">
+                  <div className="flex">
+                    <div className="flex items-center">
+                      <i class="fa-regular fa-file-image text-xl logo1"></i>
+                      <p className="ml-2">Photo</p>
+                    </div>
+                    <div className="flex items-center ml-10">
+                      <i class="fa-regular fa-file-video text-xl logo1"></i>
+                      <p className="ml-2">Video</p>
+                    </div>
+                  </div>
+                  <div>
+                    <button className="bg-questions pt-2 pb-2 pr-10 pl-10 rounded-3xl font-semibold">
+                      Post
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="ml-20 mr-20 mt-12">
-          <div className="flex shadow-lg rounded-[20px] w-[100%] h-[350px]">
-
-          </div>
-        </div>
-      </div>
-      <div className="h-100 border border-gray-200 hidden xl:block">
-      </div>
-      <div className="xl:w-[25%]">
-        <div className="">
-          <div>
-            <p className="m-10 mb-4 font-semibold text-2xl">Notifications</p>
+          <div className="flex justify-between items-center ml-10 mr-10 mt-[8%] mb-7">
+            <div className="flex items-center ">
+              <p className="text-lg font-bold text-xl">All Questions</p>
+            </div>
+            <div>
+              <button className="catergory pr-4 pl-4 rounded-3xl font-semibold mr-5">
+                Newest
+              </button>
+              <button className="catergory pr-4 pl-4 rounded-3xl font-semibold">
+                Popular
+              </button>
+            </div>
           </div>
           <div className="flex flex-col items-center ml-2">
-            <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
-              <div className="flex items-center">
-              <div>
-              <div className="ml-4 w-2 h-2 bg-gray-400 rounded">
+            <div className="xl:w-[90%] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center mt-4">
+                  <div>
+                    <div className="ml-4 w-2 h-2 bg-green-700 rounded"></div>
+                  </div>
+                  <div>
+                    <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black"></div>
+                  </div>
+                  <div className="flex flex-col ml-2">
+                    <p className="text-lg font-semibold ">Thushanka Shyamal</p>
+                    <p className="text-gray-400">Online</p>
+                  </div>
+                </div>
+                <div className="flex ml-20 mr-5">
+                    <i class="text-gray-400 fa-solid fa-angle-down text-2xl"></i>
+                </div>
               </div>
-              </div>
-              <div>
-              <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black">
-              </div>
-              </div>
-              <div className="flex flex-col ml-2">
-                <p className="text-lg font-semibold ">Pramukha Thenuwara</p>
-                <p className="opacity-40">added a comment 
-                to your post.</p>
-              </div>
-              </div>
-            </div>
-            <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
-              <div className="flex items-center">
-              <div>
-              <div className="ml-4 w-2 h-2 bg-gray-400 rounded">
-              </div>
-              </div>
-              <div>
-              <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black">
-              </div>
-              </div>
-              <div className="flex flex-col ml-2">
-                <p className="text-lg font-semibold ">Thushanka Pramuditha</p>
-                <p className="opacity-40">added a comment 
-                to your post.</p>
-              </div>
-              </div>
-            </div>
-            <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
-              <div className="flex items-center">
-              <div>
-              <div className="ml-4 w-2 h-2 bg-gray-400 rounded">
-              </div>
-              </div>
-              <div>
-              <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black">
-              </div>
-              </div>
-              <div className="flex flex-col ml-2">
-                <p className="text-lg font-semibold ">Dinushanka Shyamal</p>
-                <p className="opacity-40">added a comment 
-                to your post.</p>
-              </div>
+              <div className="">
+                <div className="border-l border-gray-500 h-auto ml-14 mt-4 p-2 mr-10  mb-3">
+                  <div className="pl-2"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quia consectetur debitis id natus, repellat dicta fuga perferendis, porro aperiam tenetur dolores accusantium enim consequuntur repudiandae quod assumenda commodi nisi.</p></div>
+                  <div className="flex pl-2 mt-5">
+                    <div className="flex items-center">
+                      <i class="fa-regular fa-message text-gray-400"></i>
+                      <p className="text-gray-400 ml-2">4</p>
+                    </div>
+                    <div className="flex items-center ml-6">
+                      <i class="fa-regular fa-eye text-gray-400"></i>
+                      <p className="text-gray-400 ml-2">4</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
-              <div className="flex items-center">
-              <div>
-              <div className="ml-4 w-2 h-2 bg-gray-400 rounded">
+            <div className="xl:w-[90%] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center mt-4">
+                  <div>
+                    <div className="ml-4 w-2 h-2 bg-green-700 rounded"></div>
+                  </div>
+                  <div>
+                    <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black"></div>
+                  </div>
+                  <div className="flex flex-col ml-2">
+                    <p className="text-lg font-semibold ">Thushanka Shyamal</p>
+                    <p className="text-gray-400">Online</p>
+                  </div>
+                </div>
+                <div className="flex ml-20 mr-5">
+                    <i class="text-gray-400 fa-solid fa-angle-down text-2xl"></i>
+                </div>
               </div>
-              </div>
-              <div>
-              <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black">
-              </div>
-              </div>
-              <div className="flex flex-col ml-2">
-                <p className="text-lg font-semibold ">Kasun Udara</p>
-                <p className="opacity-40">added a comment 
-                to your post.</p>
-              </div>
+              <div className="">
+                <div className="border-l border-gray-500 h-auto ml-14 mt-4 p-2 mr-10 mb-3">
+                  <div className="pl-2"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quia consectetur debitis id natus, repellat dicta fuga perferendis, porro aperiam tenetur dolores accusantium enim consequuntur repudiandae quod assumenda commodi nisi.</p></div>
+                  <div className="flex pl-2 mt-5">
+                    <div className="flex items-center">
+                      <i class="fa-regular fa-message text-gray-400"></i>
+                      <p className="text-gray-400 ml-2">4</p>
+                    </div>
+                    <div className="flex items-center ml-6">
+                      <i class="fa-regular fa-eye text-gray-400"></i>
+                      <p className="text-gray-400 ml-2">4</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    
+        <div className="h-100 border border-gray-200 hidden xl:block"></div>
+        <div className="xl:w-[25%]">
+          <div className="">
+            <div>
+              <p className="m-10 mb-4 font-semibold text-2xl">Notifications</p>
+            </div>
+            <div className="flex flex-col items-center ml-2">
+              <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
+                <div className="flex items-center">
+                  <div>
+                    <div className="ml-4 w-2 h-2 bg-gray-400 rounded"></div>
+                  </div>
+                  <div>
+                    <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black"></div>
+                  </div>
+                  <div className="flex flex-col ml-2">
+                    <p className="text-lg font-semibold ">Pramukha Thenuwara</p>
+                    <p className="opacity-40">added a comment to your post.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
+                <div className="flex items-center">
+                  <div>
+                    <div className="ml-4 w-2 h-2 bg-gray-400 rounded"></div>
+                  </div>
+                  <div>
+                    <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black"></div>
+                  </div>
+                  <div className="flex flex-col ml-2">
+                    <p className="text-lg font-semibold ">
+                      Thushanka Pramuditha
+                    </p>
+                    <p className="opacity-40">added a comment to your post.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
+                <div className="flex items-center">
+                  <div>
+                    <div className="ml-4 w-2 h-2 bg-gray-400 rounded"></div>
+                  </div>
+                  <div>
+                    <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black"></div>
+                  </div>
+                  <div className="flex flex-col ml-2">
+                    <p className="text-lg font-semibold ">Dinushanka Shyamal</p>
+                    <p className="opacity-40">added a comment to your post.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center xl:w-[90%] w-[350px] h-auto bg-gray-100 rounded-[10px] justify-between m-2 p-2">
+                <div className="flex items-center">
+                  <div>
+                    <div className="ml-4 w-2 h-2 bg-gray-400 rounded"></div>
+                  </div>
+                  <div>
+                    <div className="ml-3 w-[52px] h-[52px] border-2 border-yellow-400 rounded-full bg-black"></div>
+                  </div>
+                  <div className="flex flex-col ml-2">
+                    <p className="text-lg font-semibold ">Kasun Udara</p>
+                    <p className="opacity-40">added a comment to your post.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
