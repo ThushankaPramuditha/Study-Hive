5a6ee5a0323a303d2eebacc323307e6a5ed5da29
Thenuwara <pramukhapoo@gmail.com>
2024-07-31 02:52:56 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ import Login1 from './components/Login1';
 import Forums from './components/Forums';
 import Logout from './components/Logout';
 import ProfileSetup1 from './components/ProfileSetup1';
+import Settings from './components/Settings';
+import FindPartner from './components/FindPartner';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 
@@ -46,7 +48,8 @@ function App() {
           <Route path="/login1" element={<Login1 />} />
           <Route path="/forums" element={<Forums />} />
           <Route path="/logout" element={<Logout />} />
-        
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/findpartner" element={<FindPartner />} />
           
 
         </Routes>
